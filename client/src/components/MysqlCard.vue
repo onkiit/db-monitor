9fe1479a05985ed2c58ced64b32e90d4418bb4ab
Tikno <tikno.edu@gmail.com>
2018-11-28 16:53:55 +0700

    added client

---
+++ client/src/components/MysqlCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <span class="title">MySQL</span>
+        </v-card-title>
+        <v-card-text>
+            <v-layout row wrap>
+                <v-flex xs12>
+                    <v-flex xs6>
+                        <span class="body-2">Version</span>
+                    </v-flex>
+                    <v-flex xs6>
+                        <span class="caption">{{ monitoringData.version }}</span>
+                    </v-flex>
+                </v-flex>
+                <v-flex xs12>
+                    <v-flex xs6>
+                        <span class="body-2">Active Clients</span>
+                    </v-flex>
+                    <v-flex xs6>
+                        <span class="caption">{{ monitoringData.active_client }}</span>
+                    </v-flex>
+                </v-flex>
+            </v-layout>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            monitoringData: {
+                version: "",
+                active_client: 0
+            }
+        }
+    },
+    created(){
+        this.monitoringData.version = "PostgreSQL version 3"
+    },
+    methods: {
+        
+    }
+}
+</script>
---
+++ client/src/components/RedisCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <span class="title">Redis</span>
+        </v-card-title>
+        <v-card-text>
+           <v-layout row wrap>
+                <v-flex xs12>
+                    <v-flex xs6>
+                        <span class="body-2">Version</span>
+                    </v-flex>
+                    <v-flex xs6>
+                        <span class="caption">{{ monitoringData.version }}</span>
+                    </v-flex>
+                </v-flex>
+                <v-flex xs12>
+                    <v-flex xs6>
+                        <span class="body-2">Active Clients</span>
+                    </v-flex>
+                    <v-flex xs6>
+                        <span class="caption">{{ monitoringData.active_client }}</span>
+                    </v-flex>
+                </v-flex>
+            </v-layout>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            monitoringData: {}
+        }
+    },
+    created(){
+        this.getData()
+    },
+    methods: {
+        getData(){
+            fetch("http://localhost:8180/getredis")
+            .then(resp => {
+                console.log(resp)
+                this.monitoringData = resp.data
+            })
+        }
+    }
+}
+</script>
